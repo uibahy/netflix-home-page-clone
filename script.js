e90af826db4c1ac0c58faa1323aa06d783486ecb
bahy <68196786+uibahy@users.noreply.github.com>
2022-03-06 15:03:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const container = document.querySelector('.preloader');
-
-window.addEventListener('load' , () =>{
-    setTimeout( () => document.querySelector('.preloader').classList.add('hide-preloader'), 2.5*1000);
-  })
